1bbba0e88b9cbadd8603d1d60346835cf7e42d5b
Yoshito Sudo <sudo@appirio.com>
2015-03-16 09:43:37 -0700

    merge conflict cleanup

--- tech.core/tech.core.sample.dropwizard/src/main/java/com/appirio/tech/core/service/identity/IdentityConfiguration.java
+++ tech.core/tech.core.sample.dropwizard/src/main/java/com/appirio/tech/core/service/identity/IdentityConfiguration.java
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
-=======
 /**
  * 
  */
->>>>>>> sudo-v3api
 package com.appirio.tech.core.service.identity;
 
 import com.appirio.tech.core.api.v3.dropwizard.APIBaseConfiguration;
 
-<<<<<<< HEAD
-=======
 /**
  * @author sudo
  *
  */
->>>>>>> sudo-v3api
 public class IdentityConfiguration extends APIBaseConfiguration {
 
 }
